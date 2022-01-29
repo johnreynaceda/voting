290e34042a8bf0a084343c33fa6ca310d3b71fe4
johnreynaceda <johnreynaceda@sksu.edu.ph>
2022-01-29 22:14:16 +0800

    Final Commit

--- resources/views/livewire/report.blade.php
+++ resources/views/livewire/report.blade.php
@@ -209,7 +209,7 @@
                     <select id="location" name="location" wire:model="tabulate"
                         class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
                         <option value="1">All</option>
-                        <option value="2">Winner</option>
+                        {{-- <option value="2">Winner</option> --}}
 
 
                     </select>
